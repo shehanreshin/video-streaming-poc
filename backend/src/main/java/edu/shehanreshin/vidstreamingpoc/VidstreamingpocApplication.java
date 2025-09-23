67cf3d5b734bb36515155f0ede6abf213b8f3fb0
shehanreshin <asp.shehanreshin@gmail.com>
2025-09-23 14:01:04 +0530

    Initialize backend repository

---
+++ backend/src/main/java/edu/shehanreshin/vidstreamingpoc/VidstreamingpocApplication.java
@@ -0,0 +1,13 @@
+package edu.shehanreshin.vidstreamingpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidstreamingpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidstreamingpocApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/shehanreshin/vidstreamingpoc/VidstreamingpocApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.shehanreshin.vidstreamingpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidstreamingpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
